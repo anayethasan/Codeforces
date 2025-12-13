097858ae3bdbbe54e39dd6a6d0281f0cd08365aa
anayethasan <mdneloy256@gmail.com>
2025-12-13 20:04:01 +0600

    todays code added

--- 03-11-2025/Building_Teams.cpp
+++ 03-11-2025/Building_Teams.cpp
@@ -1,12 +1,12 @@
 // #include <bits/stdc++.h>
 // using namespace std;
 
-// /************************************************************
-// *                                                          *
-// *  "If talent doesn't work, believe in yourself and        *
-// *   do hard work. Allah will give you the best gift."      *
-// *                                                          *
-// *************************************************************/
+/************************************************************
+*                                                          *
+*  "If talent doesn't work, believe in yourself and        *
+*   do hard work. Allah will give you the best gift."      *
+*                                                          *
+*************************************************************/
 // #define ll long long
 // #define hea cout << "YES\n";
 // #define na cout << "NO\n";
