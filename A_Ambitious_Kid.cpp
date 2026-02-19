5b24305fff8205a62181ee9b9b7d9decb87f3f58
anayethasan <mdneloy256@gmail.com>
2026-02-20 01:19:45 +0600

    toady's code 02-20-2026

---
+++ A_Ambitious_Kid.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/************************************************************
+*                                                          *
+*  "If talent doesn't work, believe in yourself and        *
+*   do hard work. Allah will give you the best gift."      *
+*                                                          *
+*************************************************************/
+
+#define ll long long
+#define hea cout << "YES\n";
+#define na cout << "NO\n";
+#define nl cout << '\n';
+
+int main() 
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    // sieve(); 
+
+    int n;
+    cin >> n;
+    int ans = INT_MAX;
+    for(int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        ans = min(abs(x), ans);
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
