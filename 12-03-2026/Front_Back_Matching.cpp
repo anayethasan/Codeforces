94f12120656c8b224a6ce530f5f3c21d0b35c3a2
anayethasan <mdneloy256@gmail.com>
2026-03-12 01:55:03 +0600

    today's code added 12-03-2026

---
+++ 12-03-2026/Front_Back_Matching.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/************************************************************
+*                                                          *
+*  "If talent doesn't work, believe in yourself and        *
+*   do hard work. Allah will give you the best gift."      *
+*                                                          *
+*************************************************************/
+
+#define ll long long
+#define hea cout << "YES\n";
+#define na cout << "NO\n";
+#define nl cout << '\n';
+
+int main() 
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    // sieve(); 
+
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        int n;
+        string s;
+        cin >> n >> s;
+
+        vector<int> freq(26, 0);
+        for(auto ch: s)
+            freq[ch - 'a']++;
+
+        int ok = false;
+        for(int i = 0; i < 26; i++)
+        {
+            if(freq[i] >= 2)
+            {
+                ok = true;
+                break;
+            }
+        }
+
+        cout << (ok ? "Yes" : "No") << '\n';
+    }
+
+    return 0;
+}
