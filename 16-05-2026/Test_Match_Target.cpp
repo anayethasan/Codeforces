803d57e40c6a58e24be49712c02d15906a6e652f
anayethasan <mdneloy256@gmail.com>
2026-05-16 14:50:22 +0600

    today's code added

---
+++ 16-05-2026/Test_Match_Target.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/************************************************************
+*                                                          *
+*  "If talent doesn't work, believe in yourself and        *
+*   do hard work. Allah will give you the best gift."      *
+*                                                          *
+*************************************************************/
+
+#define ll long long
+#define hea cout << "YES\n";
+#define na cout << "NO\n";
+#define nl cout << '\n';
+
+int main() 
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    // sieve(); 
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if((a + c) < b)
+        cout << 0 << '\n';
+    else
+        cout << ((a + c) - b) + 1 << '\n';
+
+    return 0;
+}
