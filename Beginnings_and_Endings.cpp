c1fc9bb2734d6b7b8d9f3f19d6a712b87a9b1f08
anayethasan <mdneloy256@gmail.com>
2026-05-21 22:42:31 +0600

    today's code added

---
+++ Beginnings_and_Endings.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/************************************************************
+*                                                          *
+*  "If talent doesn't work, believe in yourself and        *
+*   do hard work. Allah will give you the best gift."      *
+*                                                          *
+*************************************************************/
+
+
+#define ll long long
+#define hea cout << "YES\n";
+#define na cout << "NO\n";
+#define nl cout << '\n';
+
+int main() 
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    // sieve(); 
+
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        int n;
+        cin >> n;
+
+        vector<int> ar(n);
+        for(int i = 0; i < n; i++)
+        {
+            cin >> ar[i];
+        }
+
+        int ans = INT_MAX;
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i + 1; j < n; j++)
+            {
+                if(ar[i] == ar[j])
+                    ans = min(ans, i + (n - 1 - j));
+            }
+        }
+
+        if(ans == INT_MAX)
+            cout << -1 << '\n';
+        else
+            cout << ans << '\n';
+    }
+
+    return 0;
+}
